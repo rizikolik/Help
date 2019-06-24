bf3493d031ba4d934f95204cb527448aee047f2f
Yunusture <46383770+rizikolik@users.noreply.github.com>
2019-06-24 18:24:00 +0300

    logo
    Logo's added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import './App.css';
-import BusinessList from './components/businessList/BusinessList';
-import SearchBar from './components/searchBar/searchBar';
-class App extends React.Component{
-  render(){
-
+import React from "react";
+import "./App.css";
+import BusinessList from "./components/businessList/BusinessList";
+import SearchBar from "./components/searchBar/searchBar";
+class App extends React.Component {
+  render() {
     return (
       <div className="App">
-    <h1>help</h1>
-    <SearchBar />
-    <BusinessList />
-  </div>
+        <h1 style={{ "vertical-align": "middle" }}>
+          helpp!
+          <img
+            style={
+              ({ width: 2 + "em" },
+              { height: 3 + "em" },
+              { "vertical-align": -20 + "px" })
+            }
+            src="https://s3-media0.fl.yelpcdn.com/assets/public/yelp_burst.yelp_styleguide.yji-4374c8fd03d15660aa2c54f2ba9b43ba.svg"
+            alt=""
+          />
+        </h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
     );
-
   }
 }
-
-
-
 
 export default App;
